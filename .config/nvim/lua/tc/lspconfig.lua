6d159e8721355fae2587cd3334da6421ba8be96c
Luis Torres Cereño <torrescereno@gmail.com>
2024-04-19 18:03:07 -0400

    fix yml

--- .config/nvim/lua/tc/lspconfig.lua
+++ .config/nvim/lua/tc/lspconfig.lua
@@ -31,6 +31,10 @@ end
 function M.common_capabilities()
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
+	capabilities.textDocument.foldingRange = {
+		dynamicRegistration = false,
+		lineFoldingOnly = true,
+	}
 	return capabilities
 end
 
