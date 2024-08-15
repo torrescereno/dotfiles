6a40fbeb577175937f91933a475e2a267e1d6793
Luis Torres Cereño <torrescereno@gmail.com>
2024-08-15 13:54:23 -0400

    config wez

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,21 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Cascadia Code")
-config.color_scheme = "Gruvbox Dark (Gogh)"
-config.font_size = 11
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+-- config.font = wezterm.font("Cascadia Code")
+
+config.font_size = 14
+
+config.color_scheme = "catppuccin-mocha"
+
+-- config.window_decorations = "NONE"
+config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 10,
+	bottom = 0,
+}
 
 return config
-
