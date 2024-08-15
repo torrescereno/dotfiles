04e76503589a54559d20d5f25d3f5244cb5efaed
Luis Torres Cereño <torrescereno@gmail.com>
2024-08-15 12:47:00 -0400

    change theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,50 @@
 return {
+
+  -- GRUVBOX MATERIAL
   -- { "f4z3r/gruvbox-material.nvim" },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function(_, opts)
-      opts.transparent = true
-      opts.italic_comments = true
-      opts.borderless_telescope = false
-    end,
-  },
+
+  -- CYBERDREAM
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = function(_, opts)
+  --     opts.transparent = true
+  --     opts.italic_comments = true
+  --     opts.borderless_telescope = false
+  --   end,
+  -- },
+
+  -- TOKYONIGH
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+
+  -- CATPPUCCIN
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox-material",
-      colorscheme = "cyberdream",
+      -- colorscheme = "cyberdream",
+      -- colorscheme = "tokyonight-storm",
+      colorscheme = "catppuccin",
     },
   },
-  {
-    "mawkler/modicator.nvim",
-    dependencies = "scottmckendry/cyberdream.nvim",
-    init = function()
-      vim.o.cursorline = false
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end,
-    opts = {},
-  },
+
+  -- CYBERDREAM
+  -- {
+  --   "mawkler/modicator.nvim",
+  --   dependencies = "scottmckendry/cyberdream.nvim",
+  --   init = function()
+  --     vim.o.cursorline = false
+  --     vim.o.number = true
+  --     vim.o.termguicolors = true
+  --   end,
+  --   opts = {},
+  -- },
 }
