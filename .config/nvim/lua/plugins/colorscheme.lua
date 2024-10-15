bcbf77dca87215a395bb37f1cf6591600808e9fb
Luis Torres <torrescereno@gmail.com>
2024-10-15 00:45:37 -0300

    colorscheme nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,69 +3,27 @@ return {
   -- GRUVBOX MATERIAL
   -- { "f4z3r/gruvbox-material.nvim" },
 
-  -- CYBERDREAM
-  -- {
-  --   "scottmckendry/cyberdream.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = function(_, opts)
-  --     opts.transparent = true
-  --     opts.italic_comments = true
-  --     opts.borderless_telescope = false
-  --   end,
-  -- },
-
-  -- TOKYONIGH
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  -- },
-
   -- CATPPUCCIN
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  -- EVERFOREST
-  -- {
-  --   "sainnhe/everforest",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.g.everforest_enable_italic = true
-  --     vim.g.everforest_background = "hard"
-  --     vim.g.everforest_better_performance = 1
-  --     vim.g.everforest_diagnostic_text_highlight = 1
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = function(_, opts)
+      opts.flavour = "macchiato"
+    end,
+  },
 
   -- ONE DARK PRO
-  {
-    "navarasu/onedark.nvim",
-  },
+  -- {
+  --   "navarasu/onedark.nvim",
+  -- },
 
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox-material",
-      -- colorscheme = "cyberdream",
-      -- colorscheme = "tokyonight-storm",
-      -- colorscheme = "catppuccin",
-      -- colorscheme = "catppuccin-frappe",
-      -- colorscheme = "everforest",
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
+      -- colorscheme = "onedark",
     },
   },
-
-  -- CYBERDREAM
-  -- {
-  --   "mawkler/modicator.nvim",
-  --   dependencies = "scottmckendry/cyberdream.nvim",
-  --   init = function()
-  --     vim.o.cursorline = false
-  --     vim.o.number = true
-  --     vim.o.termguicolors = true
-  --   end,
-  --   opts = {},
-  -- },
 }
