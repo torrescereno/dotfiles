b151d7e906909400d7bd5d5961f3fa9405b660f3
Luis Torres <torrescereno@gmail.com>
2024-10-26 00:55:48 -0300

    moonfly theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -16,14 +16,21 @@ return {
   -- },
 
   -- CATPPUCCIN
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = function(_, opts)
+  --     opts.flavour = "macchiato"
+  --     -- opts.transparent_background = true
+  --   end,
+  -- },
+
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
     priority = 1000,
-    opts = function(_, opts)
-      opts.flavour = "macchiato"
-      -- opts.transparent_background = true
-    end,
   },
 
   -- ONE DARK PRO
@@ -35,8 +42,9 @@ return {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox-material",
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
       -- colorscheme = "onedark",
+      colorscheme = "moonfly",
     },
   },
 }
