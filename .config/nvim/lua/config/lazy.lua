d3dc2e10daf4c041503c65f43f54fd605b628f93
Luis Torres Cereño <torrescereno@gmail.com>
2025-02-10 22:13:55 -0300

    add refactoring

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -42,8 +42,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.ui.dashboard-nvim" },
-
-    -- { import = "lazyvim.plugins.extras.editor.refactoring" },
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
 
     { import = "plugins" },
   },
